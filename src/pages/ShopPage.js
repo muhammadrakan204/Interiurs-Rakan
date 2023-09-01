f9c329f3e876b4f14a74f711e43b14ce150be78a
muhammad rakan <muhammadrakan24@gmail.com>
2023-09-01 15:35:52 +0700

    oke selesai ini

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -7,39 +7,41 @@ const Shop = () => {
   const dataLogin = localStorage.getItem("userLogin");
   const navigate = useNavigate();
 
+  const x = window.matchMedia("(min-width: 820px)");
   useEffect(() => {
-    const bCursor = document.querySelector(".body-cursor");
-    const cCursor = document.querySelector(".child-cursor");
-    const targetCursor = document.querySelectorAll(".target-cursor");
+    if (x.matches) {
+      const bCursor = document.querySelector(".body-cursor");
+      const cCursor = document.querySelector(".child-cursor");
+      const targetCursor = document.querySelectorAll(".target-cursor");
+      document.addEventListener("mousemove", moveCursor);
 
-    document.addEventListener("mousemove", moveCursor);
+      function moveCursor(e) {
+        let x = e.clientX;
+        let y = e.clientY;
+        // console.log(x, y);
 
-    function moveCursor(e) {
-      let x = e.clientX;
-      let y = e.clientY;
-      // console.log(x, y);
+        bCursor.style.left = `${x}px`;
+        bCursor.style.right = `${x}px`;
+        bCursor.style.top = `${y}px`;
+        bCursor.style.bottom = `${y}px`;
+        cCursor.style.left = `${x}px`;
+        cCursor.style.right = `${x}px`;
+        cCursor.style.top = `${y}px`;
+        cCursor.style.bottom = `${y}px`;
+        cCursor.style.transition = "0.1s";
+      }
 
-      bCursor.style.left = `${x}px`;
-      bCursor.style.right = `${x}px`;
-      bCursor.style.top = `${y}px`;
-      bCursor.style.bottom = `${y}px`;
-      cCursor.style.left = `${x}px`;
-      cCursor.style.right = `${x}px`;
-      cCursor.style.top = `${y}px`;
-      cCursor.style.bottom = `${y}px`;
-      cCursor.style.transition = "0.1s";
-    }
+      for (let i = 0; i < targetCursor.length; i++) {
+        targetCursor[i].addEventListener("mouseover", function () {
+          bCursor.classList.add("grow");
+          cCursor.style.display = "none";
+        });
 
-    for (let i = 0; i < targetCursor.length; i++) {
-      targetCursor[i].addEventListener("mouseover", function () {
-        bCursor.classList.add("grow");
-        cCursor.style.display = "none";
-      });
-
-      targetCursor[i].addEventListener("mouseleave", function () {
-        bCursor.classList.remove("grow");
-        cCursor.style.display = "block";
-      });
+        targetCursor[i].addEventListener("mouseleave", function () {
+          bCursor.classList.remove("grow");
+          cCursor.style.display = "block";
+        });
+      }
     }
   });
 
